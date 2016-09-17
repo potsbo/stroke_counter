6804b6d9ac05c12823311e1c8e2772f0741f9cb8
Shimpei Otsubo <potsbo@gmail.com>
2016-09-18 00:02:53 +0900

    Remove has_key? method from layout class

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -2,41 +2,26 @@ require 'spec_helper'
 
 describe StrokeCounter::Keyboard::Layout do
   let(:layout) { StrokeCounter::Keyboard::Layout.new }
-  describe '#has_key?' do
-    it 'should return true for "q"' do
-      expect( layout.has_key?('q') ).to be(true)
-    end
 
-    it 'should be false q on the right side' do
-      expect( layout.has_key?('q', on: :right)).to be(false)
-    end
-
-    it 'should be false :q (symbol) on the right side' do
-      expect( layout.has_key?(:q, on: :right)).to be(false)
-    end
-
-    describe 'key :f' do
-      it 'should return true for :f (symbol) on the right side' do
-        expect( layout.has_key?(:f, on: :right) ).to be(false)
-      end
-      it 'should return true for :f (symbol) on the right side' do
-        expect( layout.has_key?(:f, on: :left) ).to be(true)
-      end
-    end
-
-    describe 'key :h' do
-      it 'should return true for :h (symbol) on the right side' do
-        expect( layout.has_key?(:h, on: :right) ).to be(true)
-      end
-      it 'should return true for :h (symbol) on the right side' do
-        expect( layout.has_key?(:h, on: :left) ).to be(false)
-      end
+  describe '#left_side_keys' do
+    it 'should contain 15 characters' do
+      expect(layout.left_side_keys.size).to be 15
     end
   end
 
-  describe '#name' do
-    it 'should be :qwerty for no initializer option' do
-      expect(layout.name).to be(:qwerty)
+  describe '#right_side_keys' do
+    it 'should contain 15 characters' do
+      expect(layout.right_side_keys.size).to be 15
+    end
+  end
+
+  describe '#keys' do
+    it 'should contain 30 characters' do
+      expect(layout.keys.size).to be 30
+    end
+
+    it 'should contain all 26 alphabets' do
+      expect((:a..:z).to_a - layout.keys).to eq []
     end
   end
 end
