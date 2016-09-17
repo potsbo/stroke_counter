047b697a96e1b797419e3bf46089acf5e1f6edd6
Shimpei Otsubo <potsbo@gmail.com>
2016-09-17 22:09:41 +0900

    Specific implementation of keyboard layout

--- lib/stroke_counter/keyboard/layout.rb
+++ lib/stroke_counter/keyboard/layout.rb
@@ -6,10 +6,31 @@ module StrokeCounter
       attr_reader :name
       def initialize(args = {} )
         @name = ( args[:name].presence || DEFAULT ).downcase.to_sym
+        initialize_with_name
       end
 
-      def has_key?(key)
-        true
+      def initialize_with_name(name = @name)
+        @rows = [
+            %i(q w e r t y u i o p),
+            %i(a s d f g h j k l ;),
+            %i(z x c v b n m , . /)
+        ].freeze
+
+        @left_side  = @rows.map { |row| row[0...5] }
+        @right_side = @rows.map { |row| row[4...9] }
+      end
+
+      def has_key?(key, on: nil)
+        case on
+          when :right
+            return @right_side.flatten.include?(key.to_sym)
+          when nil
+            return @rows.flatten.include?(key.to_sym)
+          when :left
+            return @left_side.flatten.include?(key.to_sym)
+          else
+            return false
+        end
       end
     end
   end
--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -6,6 +6,32 @@ describe StrokeCounter::Keyboard::Layout do
     it 'should return true for "q"' do
       expect( layout.has_key?('q') ).to be(true)
     end
+
+    it 'should be false q on the right side' do
+      expect( layout.has_key?('q', on: :right)).to be(false)
+    end
+
+    it 'should be false :q (symbol) on the right side' do
+      expect( layout.has_key?(:q, on: :right)).to be(false)
+    end
+
+    describe 'key :f' do
+      it 'should return true for :f (symbol) on the right side' do
+        expect( layout.has_key?(:f, on: :right) ).to be(false)
+      end
+      it 'should return true for :f (symbol) on the right side' do
+        expect( layout.has_key?(:f, on: :left) ).to be(true)
+      end
+    end
+
+    describe 'key :h' do
+      it 'should return true for :h (symbol) on the right side' do
+        expect( layout.has_key?(:h, on: :right) ).to be(true)
+      end
+      it 'should return true for :h (symbol) on the right side' do
+        expect( layout.has_key?(:h, on: :left) ).to be(false)
+      end
+    end
   end
 
   describe '#name' do
