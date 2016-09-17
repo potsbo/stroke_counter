a9327bf7cdd9440545a5e9879971e9058eb478ab
Shimpei Otsubo <potsbo@gmail.com>
2016-09-18 02:00:07 +0900

    Simple analyse method implemeted

--- lib/stroke_counter/keyboard/logger.rb
+++ lib/stroke_counter/keyboard/logger.rb
@@ -13,4 +13,11 @@ class StrokeCounter::Keyboard::Logger
       false
     end
   end
+
+  def analyze
+    return { left: nil, right: nil} if @logs.size == 0
+    left_strokes  = @logs.select { |log| log[:hand] == :left  }
+    right_strokes = @logs.select { |log| log[:hand] == :right }
+    { left: left_strokes.size / @logs.size.to_f, right: right_strokes.size / @logs.size.to_f }
+  end
 end
