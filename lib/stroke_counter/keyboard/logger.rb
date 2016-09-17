9f772efd32ea150582e91b124bd0559ded459112
Shimpei Otsubo <potsbo@gmail.com>
2016-09-18 01:38:29 +0900

    Implement add_log method

--- lib/stroke_counter/keyboard/logger.rb
+++ lib/stroke_counter/keyboard/logger.rb
@@ -1,2 +1,14 @@
 class StrokeCounter::Keyboard::Logger
+  def initialize(args = {})
+    @logs = []
+  end
+
+  def add_log(log)
+    if log.is_a? Hash
+      @logs << log
+      true
+    else
+      false
+    end
+  end
 end
