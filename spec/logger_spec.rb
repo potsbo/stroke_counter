bf898fe57b8fb6095925c7716e20539d4f11642d
Shimpei Otsubo <potsbo@gmail.com>
2016-09-18 01:35:57 +0900

    Add #add_log simple test

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -5,4 +5,10 @@ describe StrokeCounter::Keyboard::Logger do
   it 'should be able to store feedback log' do
     expect(logger).not_to be_nil
   end
+
+  describe '#add_log' do
+    it 'should store logs' do
+      expect(logger.add_log({})).to be_truthy
+    end
+  end
 end
