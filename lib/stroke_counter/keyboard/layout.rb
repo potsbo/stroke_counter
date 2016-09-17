8fbf0c1a12932585d2dff2cde463501888b32070
Shimpei Otsubo <potsbo@gmail.com>
2016-09-17 21:48:30 +0900

    Add simple test for keyboard layout

--- lib/stroke_counter/keyboard/layout.rb
+++ lib/stroke_counter/keyboard/layout.rb
@@ -1,7 +1,10 @@
 module StrokeCounter
   class Keyboard
     class Layout
-
+      def has_key?(key)
+        true
+      end
     end
   end
 end
+
--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe StrokeCounter::Keyboard::Layout do
+  let(:layout) { StrokeCounter::Keyboard::Layout.new }
+  describe '#has_key?' do
+    it 'should return true for "q"' do
+      expect( layout.has_key?('q') ).to be(true)
+    end
+  end
 end
