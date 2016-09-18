edb250724f3308267e2cbdabc49a504d3b524212
Shimpei Otsubo <potsbo@gmail.com>
2016-09-18 22:52:12 +0900

    Refactor, right and left strokes

--- lib/stroke_counter/keyboard/logger.rb
+++ lib/stroke_counter/keyboard/logger.rb
@@ -16,9 +16,19 @@ class StrokeCounter::Keyboard::Logger
 
   def analyze
     return { left: nil, right: nil} if @logs.size == 0
-    left_strokes  = @logs.select { |log| log[:hand] == :left  }
-    right_strokes = @logs.select { |log| log[:hand] == :right }
     { left: left_strokes.size / @logs.size.to_f, right: right_strokes.size / @logs.size.to_f }
+  end
+
+  def left_strokes
+    hand_strokes(:left)
+  end
+
+  def right_strokes
+    hand_strokes(:right)
+  end
+
+  def hand_strokes(hand)
+    @logs.select { |log| log[:hand] == hand}
   end
 
   def probabilities
