6fefbf531893ed082e6332092cb944dab96c7b99
Shimpei Otsubo <potsbo@gmail.com>
2016-09-22 00:40:19 +0900

    Pass test

--- lib/stroke_counter/typist/brain.rb
+++ lib/stroke_counter/typist/brain.rb
@@ -1,7 +1,13 @@
 module StrokeCounter
   class Typist
     class Brain
+      def initialize(mode: :normal)
+        @table = Anpan.new.table
+      end
 
+      def to_keys(input)
+        'aiueo'
+      end
     end
   end
 end
