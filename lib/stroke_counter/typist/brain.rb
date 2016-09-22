9df3272218d7604ccc560237853825b73b78c609
Shimpei Otsubo <potsbo@gmail.com>
2016-09-22 15:23:15 +0900

    Complex patterns and Dvorak mode supported

--- lib/stroke_counter/typist/brain.rb
+++ lib/stroke_counter/typist/brain.rb
@@ -10,7 +10,7 @@ module StrokeCounter
         keys = []
         while !input.empty?
           pat   = best_pattern_for(input)
-          keys << pat[:input].to_s if pat
+          keys << pat[:input].to_s[@rest.size..-1] if pat
           input = input[pat[:output].size..-1]
         end
         keys.join
@@ -18,11 +18,25 @@ module StrokeCounter
 
       def best_pattern_for(input)
         pats = @table.select do |pattern|
+          pattern[:input].to_s.start_with? @rest.to_s
+        end
+        pats = pats.select do |pattern|
           input.start_with? pattern[:output]
         end
-        pats.reverse!
-        pats = pats.sort_by { |pattern| pattern[:output].to_s.size / pattern[:input].to_s.size.to_f }
-        pats.last
+        pats = pats.select do |pattern|
+          compatible_with_next(input[pattern[:output].size..-1].to_s,pattern[:addition].to_s)
+        end
+        pat = pats.max_by { |pattern| pattern[:output].to_s.size / pattern[:input].to_s.size.to_f }
+        @rest = pat[:addition]
+        pat
+      end
+
+      def compatible_with_next(input, addition)
+        return true if input.empty?
+        pats = @table.select do |pattern|
+          input.start_with? pattern[:output]
+        end
+        pats.find { |pat| pat[:input].to_s.start_with? addition.to_s}
       end
     end
   end
--- spec/brain_spec.rb
+++ spec/brain_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe StrokeCounter::Typist::Brain do
-  let(:brain) { StrokeCounter::Typist::Brain.new }
+  let(:brain) { StrokeCounter::Typist::Brain.new(mode: mode) }
+  let(:mode) { :normal }
   describe '#to_keys' do
     let(:keys) { brain.to_keys(input) }
     let(:input) { "" }
@@ -48,5 +49,60 @@ describe StrokeCounter::Typist::Brain do
       end
     end
 
+    describe 'normal sentence' do
+      test_cases = {
+        'きょうはめちゃくちゃいいてんきってかんじですね。' => 'kyouhametyakutyaiitenkittekanzidesune.',
+      }
+      test_cases.each do |jp,en|
+        context "when '#{jp}' given" do
+          let(:input) { jp }
+          it "should return #{en}" do
+            expect(keys).to eq en
+          end
+        end
+      end
+    end
+
+    context 'when mode is dvorak' do
+      let(:mode) { :dvorak }
+      describe 'one to one patterns' do
+        test_cases = {
+          'あいうえお' => "'ueo",
+          'かきくけこ' => 'cacicuceco',
+          'さしすせそ' => 'sasisuseso',
+          'たちつてと' => 'tatituteto',
+          'なにぬねの' => 'naninuneno',
+          'はひふへほ' => 'hahihuheho',
+          'まみむめも' => 'mamimumemo',
+          'やゆよ'    => 'fafufo',
+          'らりるれろ' => 'rarirurero',
+          'わをん'    => 'wawq',
+        }
+        test_cases.each do |jp,en|
+          context "when '#{jp}' given" do
+            let(:input) { jp }
+            it "should return #{en}" do
+              expect(keys).to eq en
+            end
+          end
+        end
+      end
+
+      describe 'normal sentence' do
+        test_cases = {
+          'きょうはめちゃくちゃいいてんきってかんじですね。' => "cn,hametnacutn'itjcittec;zidesunel.",
+          'がっつりけいしょくひんだいこうひょうはつばいちゅう。' => "gatturic.shocuhxd'c,hn,hatub'tnpl.",
+          'あいんしゅたいんはかみれべるのぶつりがくしゃ' => "'xshutaxhacamireberunobuturigacusha",
+        }
+        test_cases.each do |jp,en|
+          context "when '#{jp}' given" do
+            let(:input) { jp }
+            it "should return #{en}" do
+              expect(keys).to eq en
+            end
+          end
+        end
+      end
+    end
   end
 end
