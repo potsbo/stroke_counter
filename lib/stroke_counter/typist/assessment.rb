e67b94cf65df588334a39fedfc994b75dd29ea10
Shimpei Otsubo <potsbo@gmail.com>
2016-09-28 17:01:16 +0900

    Fix to accept 0 args

--- lib/stroke_counter/typist/assessment.rb
+++ lib/stroke_counter/typist/assessment.rb
@@ -1,5 +1,5 @@
 module StrokeCounter::Typist::Assessment
-  def path_to_file(filename)
+  def path_to_file(filename = nil)
     filename ||= 'constitution_of_japan.txt'
     File.join 'lib', 'stroke_counter', 'texts', filename
   end
