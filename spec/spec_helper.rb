10ce193b531db50f64306478370b51ab81d5bac1
Shimpei Otsubo <potsbo@gmail.com>
2016-10-03 09:31:27 +0900

    Require codecov in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "stroke_counter"
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
