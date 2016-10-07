83217a9c4b6aecb56ff4329280de0f9875ad67ed
Shimpei Otsubo <potsbo@gmail.com>
2016-10-08 04:04:56 +0900

    Nested

--- lib/stroke_counter/keyboard/layout/dvorak.rb
+++ lib/stroke_counter/keyboard/layout/dvorak.rb
@@ -1,9 +1,11 @@
-class StrokeCounter::Keyboard
-  class Layout
-    DVORAK = [
-      %i(' , . p y f g c r l),
-      %i(a o e u i d h t n s),
-      %i(; q j k x b m w v z),
-    ].freeze
+module StrokeCounter
+  class Keyboard
+    class Layout
+      DVORAK = [
+        %i(' , . p y f g c r l),
+        %i(a o e u i d h t n s),
+        %i(; q j k x b m w v z),
+      ].freeze
+    end
   end
 end
