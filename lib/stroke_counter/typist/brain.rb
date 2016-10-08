28feacdf3421039bc02aa71ac7fa344c5cad0120
Shimpei Otsubo <potsbo@gmail.com>
2016-10-08 14:23:59 +0900

    Simple assignment

--- lib/stroke_counter/typist/brain.rb
+++ lib/stroke_counter/typist/brain.rb
@@ -1,7 +1,6 @@
 module StrokeCounter
   class Typist
     class Brain
-      class NoCompatiblePattern < StandardError; end
       CONF = {
         anpan: Anpan::An::CONF,
         google: Anpan::An::GOOGLE_JAPANESE,
@@ -21,12 +20,8 @@ module StrokeCounter
       def to_keys(input)
         keys = []
         until input.empty?
-          begin
-            pat = best_pattern_for(input: input)
-          rescue
-            pat = { input: input[0], output: input[0] }
-          end
-          keys << pat[:input].to_s[@rest.to_s.size..-1] if pat
+          pat = best_pattern_for(input: input) || { input: input[0], output: input[0] }
+          keys << pat[:input].to_s[@rest.to_s.size..-1]
           input = input[pat[:output].size..-1]
         end
         keys.join
@@ -48,13 +43,11 @@ module StrokeCounter
         end
         pat = pats.max_by { |pattern| efficiency_with_next(input: input, pattern: pattern) }
         @rest = pat.nil? ? nil : pat[:addition]
-        raise NoCompatiblePattern, "No compatible pattern for '#{input[0]}'" if pat.nil?
         pat
       end
 
       def efficiency_with_next(input: '', pattern: {})
-        next_input = input[pattern[:output].size..-1]
-        next_pattern = best_score_pattern(patterns: compatible_patterns(input: next_input))
+        next_pattern = best_score_pattern(patterns: compatible_patterns(input: input[pattern[:output].size..-1]))
         next_output  = next_pattern.nil? ? 0 : next_pattern[:output].to_s
         next_input   = next_pattern.nil? ? 0 : next_pattern[:input].to_s
         (next_output.size + pattern[:output].to_s.size) / (next_input.size + pattern[:input].to_s.size).to_f
