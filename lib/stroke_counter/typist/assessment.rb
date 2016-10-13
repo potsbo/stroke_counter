ae8e1182e5acca235c9417f74d3162eaaff55222
Shimpei Otsubo <potsbo@gmail.com>
2016-10-13 15:49:44 +0900

    Use constitution of the united states when :en lang given

--- lib/stroke_counter/typist/assessment.rb
+++ lib/stroke_counter/typist/assessment.rb
@@ -1,6 +1,10 @@
 module StrokeCounter
   class Typist
     module Assessment
+      FILENAMES = {
+        en: 'constitution_of_the_united_states.txt',
+        ja: 'constitution_of_japan.txt',
+      }
       def path_to_file(filename = nil)
         filename ||= 'constitution_of_japan.txt'
         File.join 'lib', 'stroke_counter', 'texts', filename
@@ -10,7 +14,8 @@ module StrokeCounter
         File.new(path_to_file(filename), 'r')
       end
 
-      def assess(filename: nil)
+      def assess(filename: nil, lang: nil)
+        filename = FILENAMES[lang.to_sym] if lang
         text_file(filename: filename).each_line do |line|
           type_language(line)
         end
